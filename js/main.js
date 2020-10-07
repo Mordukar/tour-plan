93dc57e3b68f36925ca3a4498126749c991056b3
BezhenarMordukar <t80_01@mail.ru>
2020-10-07 19:14:30 +0300

    create: validate

--- js/main.js
+++ js/main.js
@@ -71,4 +71,23 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+  //Обработка форм
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "The name should not be shorter than 2 letters",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Please specify your phone",
+        },
+      },
+    });
+  });
 });
